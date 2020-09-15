9cf99398d9fb241d843ac9b9524ba16f14023264
root <root@DESKTOP-4I5C2KQ.localdomain>
2020-09-15 14:22:37 -0500

    Ejercicio 5 Express

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  for (let i = 1; i <= 50; i++) {
+    if (i % 2 === 0) {
+      res.write(`<p>${i} Soy Par!</p>`)
+    } else {
+      res.write(`<p>${i} Soy Impar!</p>`)
+    }
+  }
+  res.end()
+})
+
+app.listen(9000, () => console.log('Listening on port 9000!'))
